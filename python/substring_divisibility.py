a425d1d775e519d7bcb2f0491006f55cba4f5512
Supriya Sudarshan <47180439+supria68@users.noreply.github.com>
2020-06-09 17:21:24 +0200

    EP - 43 (Substring divisibility)

---
+++ python/substring_divisibility.py
@@ -0,0 +1,35 @@
+"""
+EP - 43
+
+The number, 1406357289, is a 0 to 9 pandigital number because it is made up of each of the digits 0 to 9 in some order, but it also has a rather interesting sub-string divisibility property.
+
+Let d1 be the 1st digit, d2 be the 2nd digit, and so on. In this way, we note the following:
+
+d2d3d4=406 is divisible by 2
+d3d4d5=063 is divisible by 3
+d4d5d6=635 is divisible by 5
+d5d6d7=357 is divisible by 7
+d6d7d8=572 is divisible by 11
+d7d8d9=728 is divisible by 13
+d8d9d10=289 is divisible by 17
+Find the sum of all 0 to 9 pandigital numbers with this property.
+"""
+
+#Let's make use of python itertools to get all pandigital numbers
+import itertools
+
+if __name__ == "__main__":
+
+    prime_list = [2, 3, 5, 7, 11, 13, 17]
+
+    def isSubstrDivisible(num):
+        # checks if all sub strings of a pandigital number are divisible by prime number
+        return all((num[i + 1] * 100 + num[i + 2] * 10 + num[i + 3]) % p == 0 for (i, p) in enumerate(prime_list))
+     
+    panlist = [] #holds pandigital numbers with substring divisibility!
+    for num in itertools.permutations(list(range(10))):
+        if isSubstrDivisible(num):
+            panlist.append("".join(map(str,num)))
+    
+    print(sum(map(int,panlist))) 
+    
